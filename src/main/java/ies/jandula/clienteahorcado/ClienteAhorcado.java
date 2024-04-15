05680b4b8aad3d93892adb783b21474e16957014
jjurval099 <jjurval099@g.educaand.es>
2024-04-15 11:15:29 +0200

    Servidor envia las letras

--- src/main/java/ies/jandula/clienteahorcado/ClienteAhorcado.java
+++ src/main/java/ies/jandula/clienteahorcado/ClienteAhorcado.java
@@ -1,10 +1,78 @@
 package ies.jandula.clienteahorcado;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+import ies.jandula.servidorahorcado.ServidorAhorcado;
+
 public class ClienteAhorcado 
 {
-	public static void main(String[] args)
-	{
+	public static final String HOST = "localhost";
 
-	}
+    public static void main(String[] args) 
+    {
+    	
+    	Socket socketCliente = null;
+        DataInputStream flujoEntrada = null;
+        DataOutputStream flujoSalida = null;
+        Scanner scanner = null;
 
+        try 
+        {
+            socketCliente = new Socket(HOST, ServidorAhorcado.PUERTO);
+            flujoEntrada = new DataInputStream(socketCliente.getInputStream());
+            flujoSalida = new DataOutputStream(socketCliente.getOutputStream());
+            scanner = new Scanner(System.in);
+                                    
+            
+            System.out.println("Introduce ELEGIR_LETRA para comenzar");
+            
+            flujoSalida.writeUTF(scanner.nextLine());
+            
+            System.out.println(flujoEntrada.readUTF());
+            
+            System.out.println("Letras no usadas: "+flujoEntrada.readUTF());
+            
+            System.out.println("Introduce una letra: ");
+            
+            flujoSalida.writeUTF(scanner.nextLine());
+
+            
+        } 
+        catch (Exception exception)
+        {
+            System.out.println(exception.getMessage());
+        }
+        finally
+        {
+        	try 
+        	{
+				flujoEntrada.close();
+			} 
+        	catch (IOException e) 
+        	{
+				e.printStackTrace();
+			}
+            try 
+            {
+				flujoSalida.close();
+			} 
+            catch (IOException e) 
+            {
+				e.printStackTrace();
+			}
+            try 
+            {
+				socketCliente.close();
+			} 
+            catch (IOException e) 
+            {
+				e.printStackTrace();
+			}
+            scanner.close();
+        }
+    }
 }
--- src/main/java/ies/jandula/servidorahorcado/ServidorAhorcado.java
+++ src/main/java/ies/jandula/servidorahorcado/ServidorAhorcado.java
@@ -1,10 +1,108 @@
 package ies.jandula.servidorahorcado;
 
-public class ServidorAhorcado {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class ServidorAhorcado
+{
 
-	}
+	public static final int PUERTO = 2000;
 
+    public static void main(String[] args) 
+    {
+    	ServerSocket socketServidor = null;
+    	Socket socketCliente = null;
+        DataInputStream flujoEntrada = null;
+        DataOutputStream flujoSalida = null;
+        
+        String [] palabras = {"Perro","Gato","Raton","Comadreja"};
+        
+        List<Character> letras = new ArrayList<>();
+
+        try 
+        {
+            socketServidor = new ServerSocket(PUERTO);
+            System.out.println("Escuchando en el puerto " + PUERTO);
+
+            socketCliente = socketServidor.accept();
+            flujoEntrada = new DataInputStream(socketCliente.getInputStream());
+            flujoSalida = new DataOutputStream(socketCliente.getOutputStream());
+            
+            Random random = new Random();
+            
+            int vidas = 5;
+            
+            String palabraEscogida = palabras[random.nextInt()];
+            
+            if(flujoEntrada.readUTF().equals("ELEGIR_LETRA")) 
+            {
+            	String partidaIniciada = "Comienza la partida";
+                
+                flujoSalida.writeUTF(partidaIniciada);
+                
+                for(char letra = 'a';letra<='z';letra++)
+                {
+                	letras.add(letra);
+                }
+                
+                flujoSalida.writeUTF(letras.toString());
+                
+                for(char letra = 'a'; letra <= 'z';letra++)
+                {
+                	if(letra == (Character) flujoEntrada.readUTF())
+                	{
+                		
+                	}
+                	
+                }
+                
+            }            
+            
+        } 
+        catch (Exception exception)
+        {
+            System.out.println(exception.getMessage());
+        }
+        finally
+        {
+        	try 
+        	{
+				flujoEntrada.close();
+			} 
+        	catch (IOException e) 
+        	{
+				e.printStackTrace();
+			}
+            try
+            {
+				flujoSalida.close();
+			} 
+            catch (IOException e) 
+            {
+				e.printStackTrace();
+			}
+            try 
+            {
+				socketCliente.close();
+			} 
+            catch (IOException e)
+            {
+				e.printStackTrace();
+			}
+            try 
+            {
+				socketServidor.close();
+			} 
+            catch (IOException e) 
+            {
+				e.printStackTrace();
+			}
+        }
+    }
 }
